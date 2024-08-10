51340fa5de729ccc49426392646bccac0a64cfad
StrawHat1Luffy <strawhatmonkey.d.luffy88@gmail.com>
2024-08-10 08:20:02 +0300

    Optimizing performance of e2e/clients/dashboard-vue/App.vue

---
+++ e2e/clients/dashboard-vue/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <dashboard :ImageToolLearn="ImageToolLearn" />
+</template>
+
+<script>
+import Vue from 'vue'
+import ImageToolLearn from '@ImageToolLearn/core'
+import { Dashboard } from '@ImageToolLearn/vue'
+
+export default {
+  name: 'App',
+  components: {
+    Dashboard,
+  },
+  computed: {
+    ImageToolLearn: () => new ImageToolLearn(),
+  },
+}
+</script>
+
+<style src="@ImageToolLearn/core/dist/style.css"></style>
+<style src="@ImageToolLearn/dashboard/dist/style.css"></style>
